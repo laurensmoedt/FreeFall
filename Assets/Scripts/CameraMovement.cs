623e4ebc422d40bcbd33e82fc6568ddf304d63ee
Laurens Moedt <laurensmoedt@gmail.com>
2020-09-19 11:32:41 +0200

    Initial commit
    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, GameObject.Find("Player").transform.position.y +5, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float movementSpeed;
+    float increasingFallingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += transform.TransformDirection(Vector3.down) * 5.0f;
+        increasingFallingSpeed = 0.05f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        transform.position += transform.TransformDirection(Vector3.down) * Time.deltaTime * increasingFallingSpeed;
+
+        if (Input.GetKey("w")) 
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        if (Input.GetKey("s"))
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        if (Input.GetKey("a"))
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        if (Input.GetKey("d"))
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        
+    }
+}
